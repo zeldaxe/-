fb83e296ec54d299195011579cc68a53853b34cb
zeldaxe <160448541+zeldaxe@users.noreply.github.com>
2024-03-27 23:05:21 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+import { LocalizationHelper } from '@microsoft/mgt-element';
+LocalizationHelper.strings = {
+  _components: {
+    login: {
+      signInLinkSubtitle: 'تسجيل الدخول',
+      signOutLinkSubtitle: 'خروج'
+    },
+    'people-picker': {
+      inputPlaceholderText: 'ابدأ في كتابة الاسم',
+      noResultsFound: 'لم نجد أي قنوات',
+      loadingMessage: '...جار التحميل'
+    },
+    'teams-channel-picker': {
+      inputPlaceholderText: 'حدد قناة',
+      noResultsFound: 'لم يتم العثور على نتائج',
+      loadingMessage: 'Loading...'
+    },
+    planner: {
+      removeTaskSubtitle: 'delete',
+      cancelNewTaskSubtitle: 'canceltest',
+      newTaskPlaceholder: 'newTaskTest',
+      addTaskButtonSubtitle: 'addme'
+    },
+    todo: {
+      removeTaskSubtitle: 'todoremoveTEST'
+    },
+    'person-card': {
+      showMoreSectionButton: 'أظهر المزيد' // global declaration
+    },
+    'contact': {
+      contactSectionTitle: 'اتصل'
+    },
+    'organization': {
+      reportsToSectionTitle: 'تقارير ل',
+      directReportsSectionTitle: 'تقارير مباشرة',
+      organizationSectionTitle: 'منظمة',
+      youWorkWithSubSectionTitle: 'انت تعمل مع',
+      userWorksWithSubSectionTitle: 'يعمل مع'
+    },
+    'messages': {
+      emailsSectionTitle: 'رسائل البريد الإلكتروني'
+    },
+    'file-list': {
+      filesSectionTitle: 'الملفات',
+      sharedTextSubtitle: 'مشترك',
+      showMoreSubtitle: 'Show more 📂'
+    },
+    'profile': {
+      SkillsAndExperienceSectionTitle: 'المهارات والخبرة',
+      AboutCompactSectionTitle: 'حول',
+      SkillsSubSectionTitle: 'مهارات',
+      LanguagesSubSectionTitle: 'اللغات',
+      WorkExperienceSubSectionTitle: 'خبرة في العمل',
+      EducationSubSectionTitle: 'التعليم',
+      professionalInterestsSubSectionTitle: 'المصالح المهنية',
+      personalInterestsSubSectionTitle: 'اهتمامات شخصية',
+      birthdaySubSectionTitle: 'عيد الميلاد',
+      currentYearSubtitle: 'السنة الحالية'
+    }
+  }
+};
